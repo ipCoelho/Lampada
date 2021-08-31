7542f054d55d445f3d0fffaa12df0b87951d223a
ipCoelho <21193386@CFP127ALUNO.LOCAL>
2021-08-31 15:17:21 -0300

    lampada.js : adicionado functions desligar, ligar e quebrar

--- lampada.js
+++ lampada.js
@@ -8,7 +8,10 @@ function ligarLampada() {
 function desligarLampada() {
     lampada.src = "img/desligada.jpg"
 }
-
+function quebrarLampada() {
+    lampada.src = "img/quebrada.jpg"
+}
 // events
 document.getElementById('button-ligar').addEventListener("click", ligarLampada)
 document.getElementById('button-desligar').addEventListener("click", desligarLampada)
+lampada.addEventListener("click", quebrarLampada)
