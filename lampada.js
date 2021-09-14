8dabb28d5b23e622dab32df0b0f08aeda8f1e8a0
ipCoelho <ipcoelho.personal@gmail.com>
2021-09-14 15:38:00 -0300

    Corrigido alguns bugs

--- lampada.js
+++ lampada.js
@@ -1,43 +1,63 @@
 "use strict"
 const lampada = document.getElementById('image-lampada')
 const buttonPiscar = document.getElementById('button-piscar')
+let idInterval 
 
 // functions
-function lampadaInteira(){
+function lampadaInteira() {
     return lampada.src.includes("ligada")
 }
+
 function ligarLampada() {
     if (lampadaInteira()){
         lampada.src = "img/ligada.jpg"
     }
 }
+
 function desligarLampada() {
-    if (lampadaInteira()){
+    if (lampadaInteira()) {
         lampada.src = "img/desligada.jpg"
     }
 }
-function piscarLampada() {
-    var intervaloLigar = setInterval(ligarLampada, 2000)
-    var intervaloDesligar = setInterval(desligarLampada, 999)
-    buttonPiscar.textContent = 'Parar'
-    buttonPiscar.id = 'button-parar'
-    document.getElementById('button-parar').addEventListener("click", pararPiscarLampada)
-}
-function pararPiscarLampada() {
-    clearInterval(intervaloLigar)
-    clearInterval(intervaloDesligar)
-    desligarLampada()
-    buttonPiscar.textContent = 'Piscar'
-}
+
 function quebrarLampada() {
     lampada.src = "img/quebrada.jpg"
 }
+
+function lampadaDesligada() {
+    return lampada.src.includes('desligada')
+}
+
+function pararPiscar() {
+    clearInterval(idInterval)
+}
+
+function trocarImagem() {
+    if (lampadaDesligada()) {
+        ligarLampada()
+    } else {
+        desligarLampada()
+    }
+}
+
+function piscarLampada() {
+    const piscar = document.getElementById('button-piscar')
+    if (piscar.textContent == 'Piscar') {
+        idInterval = setInterval(trocarImagem, 1000)
+        piscar.textContent = 'Parar'
+    } else {
+        pararPiscar()
+        piscar.textContent = 'Piscar'
+    }
+
+}
+
 
 // events
 document.getElementById('button-ligar').addEventListener("click", ligarLampada)
 document.getElementById('button-desligar').addEventListener("click", desligarLampada)
 document.getElementById('button-piscar').addEventListener("click", piscarLampada)
-document.getElementById('button-parar').addEventListener("click", pararPiscarLampada)
+// document.getElementById('button-parar').addEventListener("click", pararPiscarLampada)
 lampada.addEventListener("dblclick", quebrarLampada)
 lampada.addEventListener("mouseover", ligarLampada) // mouseover -> enquanto o mouse em cima.
 lampada.addEventListener("mouseout", desligarLampada) // mouseout -> no momento que o mouse sair da área do elemento. 
